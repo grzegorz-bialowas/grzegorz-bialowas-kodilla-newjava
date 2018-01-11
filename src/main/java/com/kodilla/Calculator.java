102bdb56fb8a36e42920005464a21ae4c66ac7f0
grzegorz-bialowas <grzegorz.bialowas1@gmail.com>
2018-01-11 22:42:12 +0100

    Calculator

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,18 @@
 package com.kodilla;
-
 public class Calculator {
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+    public int substractAFromB(int a, int b) {
+        return a - b;
+    }
+}
+class CalculatorProcessor
+{
+    public static void main(String[] args) {
+        Calculator simpleCalculator;
+        simpleCalculator = new Calculator();
+        int result = simpleCalculator.addAToB(5, 5);
+        System.out.println(result);
+    }
 }
