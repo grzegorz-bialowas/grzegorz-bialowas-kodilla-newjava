d2a555bd407092c3c4cce0abe63773c75395c746
“grzegorz-bialowas <grzegorz.bialowas1@gmail.com>
2018-01-21 10:44:02 +0100

    “Testy

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,10 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.lambda.Processor;
+
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
     }
 }
