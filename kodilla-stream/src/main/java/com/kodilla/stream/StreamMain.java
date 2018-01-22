72ece8e61c212a2aacd17ce8739e1e22f6d441e3
“grzegorz-bialowas <grzegorz.bialowas1@gmail.com>
2018-01-22 12:03:12 +0100

    NewModuleFirstTwo

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,32 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 
 public class StreamMain {
     public static void main(String[] args) {
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
         expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+        PoemBeautifier beauty = new PoemBeautifier ();
+        String lower = "AABBCC";
+        String higher = "aabbcc";
+        String text = "My First Lambda Text";
+        String love = "I LOVE YOU";
+        String lowerValue = lower.toLowerCase();
+        String higherValue = higher.toUpperCase();
+        String textHigher = text.toUpperCase();
+        String textLower = text.toLowerCase();
+        beauty.beautify((() -> System.out.println("")));
+        beauty.beautify((() -> System.out.println("Let's test: My first lambda text.")));
+        beauty.beautify((() -> System.out.println(lowerValue + "  " + "My first lambda text" + "   " +  higherValue )));
+        beauty.beautify((() -> System.out.println(lowerValue+ "  " + textHigher + "  " + textLower + "  " +higherValue)));
+        beauty.beautify((() -> System.out.println(love +"***"+text+"***"+love)));
+        beauty.beautify((() -> System.out.println("***" + love.toLowerCase() +"<+++>"+text.toUpperCase()+"<+++>"+love.toLowerCase() + "***")));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -2,6 +2,6 @@ package com.kodilla.stream.beautifier;
 
 public class PoemBeautifier {
     public void beautify(PoemDecorator poemDecorator){
-
+        poemDecorator.decorate();
     }
 }
