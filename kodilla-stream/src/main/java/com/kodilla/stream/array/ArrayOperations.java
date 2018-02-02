4f904e41c65bde7bf863674b8af69e2fb947d70a
“grzegorz-bialowas <grzegorz.bialowas1@gmail.com>
2018-02-02 11:02:09 +0100

    7.5 7.6

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,9 +1,10 @@
 package com.kodilla.stream.array;
+
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers){
         IntStream.range(0, numbers.length).forEach(System.out::println);
-        return IntStream.range(0, numbers.length).map(n->numbers[n]).average().getAsDouble();
+        return IntStream.range(0, numbers.length).map(n -> numbers[n]).average().getAsDouble();
     }
 }
