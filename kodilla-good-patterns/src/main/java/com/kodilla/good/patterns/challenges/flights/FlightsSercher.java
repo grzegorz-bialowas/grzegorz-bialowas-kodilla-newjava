f14b549ff2bd5fcb0d1cf514c943218349483af8
“grzegorz-bialowas <grzegorz.bialowas1@gmail.com>
2018-02-17 20:32:43 +0100

    9.5

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flights/FlightsSercher.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flights/FlightsSercher.java
@@ -14,11 +14,18 @@ public class FlightsSercher {
                 .filter(flight -> flight.getArrivalCity() == arrivalCity)
                 .collect(Collectors.toList());
     }
-    public static List<Flight> flightWithTransfer(City transferCity, City departureCity, City arrivalCity){
+    public static List<Flight> flightDirectly(City departureCity, City arrivalCity){
         return FlightsRoutes.getFlightMap().stream()
-                .filter(flight -> flight.getArrivalCity().equals(transferCity))
+                .filter(flight -> flight.getDepartureCity().equals(departureCity) && flight.getArrivalCity().equals(arrivalCity))
                 .collect(Collectors.toList());
-        if ()
+    }
+    public static List<Flight> flightWithTransfer(City departureCity, City arrivalCity) {
+        return  FlightsRoutes.getFlightMap().stream()
+                .filter(flight -> flight.getDepartureCity().equals(departureCity) && flight.getArrivalCity().equals(arrivalCity))
+                .filter(flight -> flight.getArrivalCity().equals(departureCity) && flight.getArrivalCity().equals(arrivalCity))
+                .collect(Collectors.toList());
+
+
 
 
     }
