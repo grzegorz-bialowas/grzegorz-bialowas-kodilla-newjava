56d31c94521f427ec2d7db79d0df0a5719cc8f23
“grzegorz-bialowas <grzegorz.bialowas1@gmail.com>
2018-02-21 11:58:17 +0100

    10.3, 10.4

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.portfolio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class TaskList {
+    final private List<String> tasks;
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+    public List<String> getTasks() {
+        return tasks;
+    }
+}
